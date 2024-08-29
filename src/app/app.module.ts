48f8feeb3a72a27177800fe8ba2901a36c56ecbf
MokkapalliPrudviRaj <mokkapalliprudviraj1132001@gmail.com>
2024-08-29 17:20:43 +0530

    login feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Components imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoaderComponent } from './utility/loader/loader.component';
+import { SnackbarComponent } from './utility/snackbar/snackbar.component';
+import { AuthComponent } from './components/auth/auth.component';
 import { HeaderComponent } from './components/common/header/header.component';
 import { FooterComponent } from './components/common/footer/footer.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+//Reactive form imports
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   HttpClientModule,
   provideHttpClient,
   withFetch,
 } from '@angular/common/http';
-import { HomeComponent } from './components/home/home.component';
-import { LoaderComponent } from './utility/loader/loader.component';
-import { SnackbarComponent } from './utility/snackbar/snackbar.component';
-import { AuthComponent } from './components/auth/auth.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+
+// Angular material imports
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
@@ -34,8 +51,16 @@ import { AuthComponent } from './components/auth/auth.component';
     HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+
+    MatIconModule
   ],
-  providers: [provideHttpClient(withFetch())],
+  providers: [provideHttpClient(withFetch()), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 import {
@@ -16,14 +16,21 @@ import { AuthRequest } from '../../model/AuthRequest';
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   private static ROLE_USER: string = 'USER';
   loginForm: FormGroup;
   registrationForm: FormGroup;
   isLoginMode: boolean = true;
   isLoading: boolean = false;
   errorMessage: string | null = null;
+  emailErrorMessage = signal('');
+  passwordErrorMessage = signal('');
+  confirmPasswordErrorMessage = signal('');
+
+
 
   constructor(
     private router: Router,
@@ -33,20 +40,93 @@ export class AuthComponent {
     // login form
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
     });
 
     // register form
     this.registrationForm = this.formBuilder.group(
       {
         email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required]],
-        confirmPassword: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
+        confirmPassword: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
       },
       {
         asyncValidators: [confirmPasswordAsyncValidator()],
       },
     );
+  }
+
+  ngOnInit(): void {
+    // Subscribes to value and status changes of the 'email' control
+    this.loginForm.get('email')?.valueChanges.subscribe(() => this.updateEmailErrorMessage('Login'));
+    this.loginForm.get('email')?.statusChanges.subscribe(() => this.updateEmailErrorMessage('Login'));
+
+    this.loginForm.get('password')?.valueChanges.subscribe(() => this.updatePasswordErrorMessage('Login'));
+    this.loginForm.get('password')?.statusChanges.subscribe(() => this.updatePasswordErrorMessage('Login'));
+
+    this.registrationForm.get('email')?.valueChanges.subscribe(() => this.updateEmailErrorMessage('Registration'));
+    this.registrationForm.get('email')?.statusChanges.subscribe(() => this.updateEmailErrorMessage('Registration'));
+
+    this.registrationForm.get('password')?.valueChanges.subscribe(() => this.updatePasswordErrorMessage('Registration'));
+    this.registrationForm.get('password')?.statusChanges.subscribe(() => this.updatePasswordErrorMessage('Registration'));
+
+    this.registrationForm.get('confirmPassword')?.valueChanges.subscribe(() => this.updateConfirmPasswordErrorMessage());
+    this.registrationForm.get('confirmPassword')?.statusChanges.subscribe(() => this.updateConfirmPasswordErrorMessage());
+  }
+
+  updateEmailErrorMessage(LR: any) {
+    // Updates the error message based on the validation status of the 'email' control
+    if (LR == 'Login') {
+      if (this.loginForm.get('email')?.hasError('required')) {
+        this.emailErrorMessage.set('You must enter a value');
+      } else if (this.loginForm.get('email')?.hasError('email')) {
+        this.emailErrorMessage.set('Not a valid email');
+      } else {
+        this.emailErrorMessage.set('');
+      }
+    }
+    else {
+      if (this.registrationForm.get('email')?.hasError('required')) {
+        this.emailErrorMessage.set('You must enter a value');
+      } else if (this.registrationForm.get('email')?.hasError('email')) {
+        this.emailErrorMessage.set('Not a valid email');
+      } else {
+        this.emailErrorMessage.set('');
+      }
+    }
+  }
+
+  updatePasswordErrorMessage(LR: any) {
+    // Updates the error message based on the validation status of the 'password' control
+    if (LR == 'Login') {
+      if (this.loginForm.get('password')?.hasError('required')) {
+        this.passwordErrorMessage.set('You must enter a value');
+      } else if (this.loginForm.get('password')?.hasError('minlength') || this.loginForm.get('password')?.hasError('maxlength')) {
+        this.passwordErrorMessage.set('Not a valid password');
+      } else {
+        this.passwordErrorMessage.set('');
+      }
+    }
+    else {
+      if (this.registrationForm.get('password')?.hasError('required')) {
+        this.passwordErrorMessage.set('You must enter a value');
+      } else if (this.registrationForm.get('password')?.hasError('minlength') || this.registrationForm.get('password')?.hasError('maxlength')) {
+        this.passwordErrorMessage.set('Not a valid password');
+      } else {
+        this.passwordErrorMessage.set('');
+      }
+    }
+  }
+
+  updateConfirmPasswordErrorMessage() {
+    // Updates the error message based on the validation status of the 'confirm password' control
+    if (this.registrationForm.get('confirmPassword')?.hasError('required')) {
+      this.confirmPasswordErrorMessage.set('You must enter a value');
+    } else if (this.registrationForm.get('confirmPassword')?.hasError('minlength') || this.registrationForm.get('confirmPassword')?.hasError('maxlength')) {
+      this.confirmPasswordErrorMessage.set('Not a valid confirm Password');
+    } else {
+      this.confirmPasswordErrorMessage.set('');
+    }
   }
 
   // Convenience getter for easy access to form fields
@@ -119,6 +199,25 @@ export class AuthComponent {
     setTimeout(() => {
       this.errorMessage = null;
     }, 3000);
+  }
+
+  //click event to toggle blw hide and view the input value
+  hideLogin = signal(true);
+  hideRegistrationP = signal(true);
+  hideRegistrationCP = signal(true);
+
+  clickEventLogin(event: MouseEvent) {
+    this.hideLogin.set(!this.hideLogin());
+    event.stopPropagation();
+  }
+  clickEventRegistrationP(event: MouseEvent) {
+    this.hideRegistrationP.set(!this.hideRegistrationP());
+    event.stopPropagation();
+  }
+
+  clickEventRegistrationCP(event: MouseEvent) {
+    this.hideRegistrationCP.set(!this.hideRegistrationCP());
+    event.stopPropagation();
   }
 }
 
