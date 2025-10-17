e9637111e7446f7aae8c13233f36fedddf5fc439
Peramasani Thirumala Yadav <pthirumalayadav@gmail.com>
2025-10-17 22:34:32 +0530

    auth interceptor - deprecation change

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-import { AuthInterceptor } from './app/core/auth.interceptor'; // ✅ adjust path if needed
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule),
-    provideHttpClient(withInterceptors([AuthInterceptor])), // ✅ this activates the interceptor
+    provideHttpClient(withInterceptorsFromDi()),
   ],
-}).catch(err => console.error(err));
+}).catch((err) => console.error(err));
