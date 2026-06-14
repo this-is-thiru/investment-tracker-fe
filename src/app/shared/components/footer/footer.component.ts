362a5fd26242d07d9c4ffe79007915aa60b6a1f4
MokkapalliPrudviRaj <uiuxbyprudvi@gmail.com>
2026-06-14 21:01:12 +0530

    feat: add footer component with dynamic copyright year and navigation links

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.css'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear = new Date().getFullYear();
+}
